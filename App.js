0ac89f2ab60ad13eb4b3c90d9568c78edba88049
Anirudh181013 <anirudhbvarma@gmail.com>
2025-07-30 17:03:53 +0530

    Setup Navigation and CurrenTask screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Signin from './screens/Signin';
+import StackNavigator from './navigators/StackNavigator';
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Signin/>
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <StackNavigator />
+        <StatusBar style="auto" />
+      </NavigationContainer>
     </View>
   );
 }
